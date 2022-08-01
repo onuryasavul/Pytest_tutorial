a002561c877d9c61cf491a3efb7e12ea81df1e30
onurtasavul <onuryasavul97@gmail.com>
2022-08-01 23:42:07 +0300

    __init__ error is fixed

--- src/Pytest_tutorial/guardian.py
+++ src/Pytest_tutorial/guardian.py
@@ -1,4 +1,4 @@
 class Guardian:
-    def __int__(self, first_name, last_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
