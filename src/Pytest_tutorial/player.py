628ecd9b9e1aba8cd3c9a966a20ec8a28df695b3
onurtasavul <onuryasavul97@gmail.com>
2022-08-01 23:37:43 +0300

    dataclass is added

--- src/Pytest_tutorial/player.py
+++ src/Pytest_tutorial/player.py
@@ -1,4 +1,5 @@
+from dataclasses import dataclass
+@dataclass
 class Player:
-    def __int__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+    first_name: str
+    last_name: str
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,7 @@
-from src.Pytest_tutorial.player import Player
+from src.Pytest_tutorial.player import *
 
 
 def test_construction():
     g = Player('a', 'b')
     assert 'a' == g.first_name
+    assert 'b' == g.last_name
